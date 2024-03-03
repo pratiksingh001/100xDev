e9b2ba9899c7c0d69cd527418ddce1750e7d38f7
Pratik Singh <singhpratik0001@gmail.com>
2024-03-03 17:35:29 +0530

    added zod schema

--- index.js
+++ index.js
@@ -3,19 +3,38 @@ const zod = require("zod")
 
 const app = express();
 
+const schema = zod.array(zod.number());
+
+// {
+//   email: string => email,
+//   password: atleast 8 letters,
+//   country: "IN", "US"
+// }
+
+const userSchema = zod.object({
+  email: zod.string(),
+  password: zod.string(),
+  country: zod.literal("IN").or(zod.literal("US")),
+  kidneys: zod.array(zod.number())
+})
+
 app.use(express.json()); 
 
 app.post("/health-checkup", function(req, res) {
   const kidneys = req.body.kidneys;
-  const kidneyLength = kidneys.length;
-
-  res.send(`You have ${kidneyLength} kidneys`)
+  const response = schema.safeParse(kidneys)
+  
+  if(!response.success) {
+    res.status(411).json({
+      msg: "Invalid input"
+    })
+  }else{
+    res.send({
+      response
+    })
+  }
 });
 
-app.use(function(err, req, res, next){
-  res.json({
-    msg: "Something is bad with our server"
-  })
-})
+
 
 app.listen(3000);
